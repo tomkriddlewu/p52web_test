4e094d4f44bc2db7d6d3c94ae94044ab96268fb3
tomkriddlewu <sss999369@gmail.com>
2024-05-15 10:24:24 +0800

    updateRecordFunction

--- mySketch.js.js
+++ mySketch.js.js
@@ -1,21 +1,70 @@
+let coordinate = [];
+let recording = false;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0);
+	document.getElementById('recordButton').addEventListener('click', toggleRecording);
+    document.getElementById('saveButton').addEventListener('click', saveToCSV);
 }
 
 function draw() {
-	// noFill(); 
+	// background(0, 10);
+	
+	// if(frameCount %5 == 0){
+	// 	background(0, 15);
+	// };
+	
 	noStroke();
-	fill(mouseX/4, mouseY/4, random(255));
-	print(frameCount);
-	// strokeWeight(3);
-	// stroke("white");
-	if (mouseIsPressed){
-		ellipse(mouseX, mouseY, frameCount, frameCount);
-	}else{
-		stroke(255);
-		noFill();
-		rectMode(CENTER);
-		rect(mouseX, mouseY, 20, 20);
+	fill(mouseX/4, mouseY/4, random(255), 150);
+
+	if (mouseIsPressed) {
+        ellipse(mouseX, mouseY, 150, 150);
+        if (recording) {
+            coordinate.push([mouseX, mouseY]);
+        }
+    }
+}
+
+// function keyPressed() {
+//     if (key === 'k' || key === 'K') {
+//         recording = !recording;
+//         if (recording) {
+//             console.log("開始記錄");
+//         } else {
+//             console.log("停止紀錄");
+//         }
+//     }
+//     if (key === 's' || key === 'S') {
+//         saveToCSV();
+//     }
+// }
+
+function toggleRecording() {
+	recording = !recording;
+	const recordButton = document.getElementById('recordButton');
+	if (recording) {
+		recordButton.textContent = 'Stop Recording';
+		console.log("開始記錄");
+	} else {
+		recordButton.textContent = 'Start Recording';
+		console.log("開始記錄");
 	}
+}
+
+function saveToCSV(){
+	let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += "X,Y\n";
+    coordinate.forEach(coord => {
+        csvContent += coord.join(",") + "\n";
+    });
+    
+
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "coordinates.csv");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
